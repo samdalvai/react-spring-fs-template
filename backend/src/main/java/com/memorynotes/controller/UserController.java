4da66db27c363e0388e075c05e01a67be89429a6
Samuel Dalvai <samuel.dalvai@ewo.com>
2023-03-19 20:53:14 +0000

    Log login request

--- backend/src/main/java/com/memorynotes/controller/UserController.java
+++ backend/src/main/java/com/memorynotes/controller/UserController.java
@@ -1,15 +1,13 @@
 package com.memorynotes.controller;
 
-import com.memorynotes.model.User;
+import com.memorynotes.authentication.LoginRequest;
 import com.memorynotes.repository.UserRepository;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
+import java.net.URISyntaxException;
 
 @RestController
-@RequestMapping("/login")
 public class UserController {
 
     private final UserRepository userRepository;
@@ -19,8 +17,15 @@ public class UserController {
     }
 
     // URL example: http://localhost:8080/users
-    @GetMapping
-    public List<User> getAllUsers() {
-        return userRepository.findAll();
+    @PostMapping("/login")
+    public ResponseEntity<?> createProvince(@RequestBody LoginRequest loginRequest) throws URISyntaxException {
+        System.out.println("loginrequest: " + loginRequest);
+        String email = loginRequest.getEmail();
+        String password = loginRequest.getPassword();
+
+        System.out.println("name or email: " + email);
+        System.out.println("password: " + password);
+
+        return ResponseEntity.ok("Login successful!");
     }
 }
