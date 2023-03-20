521d207b68f1337c93814e9b992523cc4dffea0b
Samuel Dalvai <samuel.dalvai@ewo.com>
2023-03-20 22:31:23 +0000

    Add indigo background for layout

--- frontend/src/layout/Layout.tsx
+++ frontend/src/layout/Layout.tsx
@@ -3,7 +3,7 @@ import { ChildrenProps } from "../types/props";
 
 export default function Layout({ children }: { children: ChildrenProps}) {
     return (
-        <div className="h-screen w-screen bg-white flex items-center justify-center">
+        <div className="h-screen bg-indigo-100 w-screen flex items-center justify-center">
             {children}
         </div>
     )
