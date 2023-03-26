d269c00690b882702eb5f0cd36657af438d2acc9
Samuel Dalvai <samuel.dalvai@ewo.com>
2023-03-26 13:44:48 +0200

    Remove lombok dependency from LoginForm

--- api-auth/src/main/java/com/shzhangji/apiauth/form/LoginForm.java
+++ api-auth/src/main/java/com/shzhangji/apiauth/form/LoginForm.java
@@ -1,12 +1,31 @@
 package com.shzhangji.apiauth.form;
 
 import jakarta.validation.constraints.NotBlank;
-import lombok.Data;
 
-@Data
 public class LoginForm {
     @NotBlank
     private String username;
     @NotBlank
     private String password;
+
+    public LoginForm(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
