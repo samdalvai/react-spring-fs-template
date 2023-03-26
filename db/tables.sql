fa99b93537dda4927312588541f255fcb59444d1
Samuel Dalvai <samuel.dalvai@ewo.com>
2023-03-26 13:03:07 +0200

    Fix dockerfile for postgreSQL to reflext test api

--- db/tables.sql
+++ db/tables.sql
@@ -12,12 +12,11 @@ INSERT INTO public.user VALUES (1, 'admin', '{bcrypt}$2a$10$f4aQLof9kgM8mzJIP7a.
 
 CREATE TABLE public.user 
 (
-    id SERIAL PRIMARY KEY,
-    name VARCHAR(50) NOT NULL,
-    email VARCHAR(50) NOT NULL UNIQUE,
-    password VARCHAR(50) NOT NULL,
-    salt VARCHAR(50) NOT NULL,
-    created_at TIMESTAMP WITH TIME ZONE NOT NULL
+  id SERIAL NOT NULL PRIMARY KEY,
+  username VARCHAR(255) NOT NULL UNIQUE,
+  password VARCHAR(255) NOT NULL,
+  nickname VARCHAR(255) NOT NULL,
+  created_at TIMESTAMP NOT NULL
 );
 
---INSERT INTO public.user VALUES (1, 'test', '{bcrypt}$2a$10$f4aQLof9kgM8mzJIP7a.Vuc3WYcQK8brcL6hrHdCdkzTH8AppEpOm', 'Jerry', NOW(), NOW());
+INSERT INTO public.user VALUES (1, 'test', '{bcrypt}$2a$10$f4aQLof9kgM8mzJIP7a.Vuc3WYcQK8brcL6hrHdCdkzTH8AppEpOm', 'Jerry', NOW());
