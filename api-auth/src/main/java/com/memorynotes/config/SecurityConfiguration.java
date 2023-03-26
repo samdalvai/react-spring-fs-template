9bddc7b7b3042fce7216037fcc2c33f1ffdf4e40
Samuel Dalvai <samuel.dalvai@ewo.com>
2023-03-26 14:14:01 +0200

    Disable password encoding

--- api-auth/src/main/java/com/memorynotes/config/SecurityConfiguration.java
+++ api-auth/src/main/java/com/memorynotes/config/SecurityConfiguration.java
@@ -9,6 +9,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.HttpStatusEntryPoint;
@@ -45,7 +46,8 @@ public class SecurityConfiguration {
 
     @Bean
     public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
+        return NoOpPasswordEncoder.getInstance(); // Use this to disable password encryption
+        //return new BCryptPasswordEncoder();
     }
 
 }
