bacbf396fa5a906330839f8555bab0edfac9af19
Samuel Dalvai <samuel.dalvai@ewo.com>
2023-03-26 13:56:23 +0200

    Add explicit password encoder

--- api-auth/src/main/java/com/shzhangji/apiauth/config/SecurityConfiguration.java
+++ api-auth/src/main/java/com/shzhangji/apiauth/config/SecurityConfiguration.java
@@ -1,11 +1,16 @@
 package com.shzhangji.apiauth.config;
 
+import com.shzhangji.apiauth.service.CustomUserDetailsService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 import org.springframework.security.web.authentication.RememberMeServices;
@@ -14,6 +19,10 @@ import org.springframework.security.web.authentication.rememberme.TokenBasedReme
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration {
+
+    @Autowired
+    private CustomUserDetailsService userDetailsService;
+
 
     @Bean("securityFilterChain")
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -33,6 +42,11 @@ public class SecurityConfiguration {
     @Bean
     RememberMeServices rememberMeServices(UserDetailsService userDetailsService) {
         return new TokenBasedRememberMeServices("demo", userDetailsService);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 
 }
