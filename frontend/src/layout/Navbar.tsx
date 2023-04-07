b777bc9659c77a926baa2b0f3ac5c4f447172c81
Samuel Dalvai <samuel.dalvai@ewo.com>
2023-04-07 18:59:41 +0200

    Add Memory notes logo on navbar

--- frontend/src/layout/Navbar.tsx
+++ frontend/src/layout/Navbar.tsx
@@ -1,6 +1,11 @@
+import MemoryLogo from "../components/MemoryLogo";
+
 export default function Navbar() {
     return (
-        <nav className="bg-gray-800 w-full h-10 absolute top-0 ">
+        <nav className="bg-gray-800 absolute top-0 w-full h-14">
+            <div className="h-full flex items-center">
+                <span className="ml-2"><MemoryLogo /></span>
+            </div>
         </nav>
     )
 }
