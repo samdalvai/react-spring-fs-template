0ea85641b7c027ddf011c801dbd95dec6a38a740
Samuel Dalvai <samuel.dalvai@ewo.com>
2023-04-07 14:05:00 +0200

    Save new user on signup

--- backend/src/main/java/com/memorynotes/controller/UserSignupController.java
+++ backend/src/main/java/com/memorynotes/controller/UserSignupController.java
@@ -1,20 +1,19 @@
 package com.memorynotes.controller;
 
 import com.memorynotes.AppException;
-import com.memorynotes.form.LoginForm;
+import com.memorynotes.authentication.CustomPasswordEncoder;
 import com.memorynotes.form.SignupForm;
 import com.memorynotes.model.User;
 import com.memorynotes.repository.UserRepository;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.validation.BindingResult;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Date;
 import java.util.Optional;
 
 @Slf4j
@@ -29,7 +28,7 @@ public class UserSignupController {
     }
 
     @PostMapping("/signup")
-    public AuthController.CurrentUser signup(@Valid @RequestBody SignupForm form) {
+    public RegisteredUser signup(@Valid @RequestBody SignupForm form) {
         log.info("New user {} signing up", form);
 
         Optional<User> existingUser = userRepository.findUserByUsernameOrEmail(form.getUsername(), form.getEmail());
@@ -39,7 +38,16 @@ public class UserSignupController {
             throw new AppException("User with name '" + form.getUsername() + "' or email '" + form.getEmail() + "' already signed up");
         }
 
-        return null;
+        PasswordEncoder encoder = CustomPasswordEncoder.getInstance();
+        String encryptedPassword = encoder.encode(form.getPassword());
+
+        User newUser = new User(form.getUsername(), form.getEmail(), encryptedPassword, new Date());
+
+        userRepository.save(newUser);
+
+        log.info("User {} successfully signed up", form);
+
+        return new RegisteredUser(newUser.getId(), newUser.getUsername(), newUser.getEmail());
     };
 
     public record RegisteredUser(Integer id, String username, String email) {
