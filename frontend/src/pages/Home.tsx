0454cde7c6438549bd1550888e0b9c54d4a0b773
Samuel Dalvai <samuel.dalvai@ewo.com>
2023-05-10 23:19:25 +0200

    Clean up home page

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,16 +1,9 @@
-import { useDispatch, useSelector } from "react-redux";
-import { RootState } from "../store";
 import Button from "../components/Button";
-import { ThunkDispatch } from "redux-thunk";
-import { logout } from "../reducers/authReducer";
 import client from "../services/client";
 import { useState } from "react";
 
 export default function Home() {
-    const dispatch = useDispatch<ThunkDispatch<any, any, any>>();
-    const { user } = useSelector((state: RootState) => state.auth);
     const [testMessage, setTestMessage] = useState<string>('');
-    console.log("User in home page: ", user)
 
     const handleTest = () => {
         client.get('api/test').then(res => {
@@ -22,13 +15,9 @@ export default function Home() {
 
     return (
         <div>
-            <div className="p-2 flex flex-col w-64 border-2 border-black shadow-md rounded">
-                <span>Hello user '{user.username}' </span>
-                <span>Test: {testMessage}</span>
-            </div>
             <Button
                 color="blue"
-                label={"Test"}
+                label={ testMessage === '' ? "Test" : testMessage}
                 name={"test"}
                 onClick={handleTest} />
         </div>
